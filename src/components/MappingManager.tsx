96afc06f65328bbfc07e69afd6c08d89ce646dee
Williaman000 <122877674+Williaman000@users.noreply.github.com>
2026-03-13 17:06:35 +0900

    fix: remove unused colors import in mapping manager

--- src/components/MappingManager.tsx
+++ src/components/MappingManager.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, type FC, type FormEvent } from 'react';
 import { useTranslation } from 'react-i18next';
 import { trProduct, trMaterial } from '../utils/dbTranslate';
-import { COLORS } from '../constants/theme';
 import type { Material, ProductMaterialMapping } from '../types';
 import type { SimpleProduct } from '../services/inventoryApi';
 import styles from './MappingManager.module.scss';
